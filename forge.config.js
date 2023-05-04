581814531a72e5e2b40b5b0e5c1922b443b61479
datistpham <giang10a1dz@gmail.com>
2023-05-04 11:03:19 +0700

    commit

---
+++ forge.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  packagerConfig: {},
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'datistpham',
+          name: 'zalo_desktop_app',
+        },
+        prerelease: false,
+        draft: true,
+      },
+    },
+  ],
+};
