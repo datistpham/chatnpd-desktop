407dc6f56412a19673e25b0f9196a3fa3527a48d
datistpham <giang10a1dz@gmail.com>
2023-05-04 12:34:59 +0700

    comit

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ const createWindow = () => {
 
   // and load the index.html of the app.
   // mainWindow.loadFile(path.join(__dirname, 'index.html'));
-  mainWindow.loadURL("chatnpd.vercel.app")
+  mainWindow.loadURL("https://chatnpd.vercel.app")
   mainWindow.on('closed', () => {
     mainWindow = null
   })
